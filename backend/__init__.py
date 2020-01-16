75e98802412f53c28d2a3f4d29130cd49a10e1e0
Ralph Kube <ralph.kube@uit.not>
2020-01-16 18:44:21 -0500

    Now serving static files that use vue.js using the blueprinted app

--- backend/__init__.py
+++ backend/__init__.py
@@ -7,18 +7,19 @@ from flask import Flask
 
 from .views.dashboard import dashboard
 # Register the dashboard blueprint in the main __init__.py:
- 
+
 
 
 def create_app(test_config=None):
     """Creates and configures the app"""
     app = Flask(__name__)
-    app.register_blueprint(dashboard, url_prefix="/backend/views")
+    app.config['TESTING'] = True
+    app.register_blueprint(dashboard, url_prefix="/")
 
     #@app.route("/hello")
     #def hello():
     #    return("Hello, World!")
- 
+    print(app.url_map)
 
     return app
 
--- backend/views/dashboard.py
+++ backend/views/dashboard.py
@@ -1,13 +1,19 @@
 # -*- Encoding: UTF-8 -*-
 # file dashboard_v2/backend/views/dashboard.py
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, send_from_directory
 
-dashboard = Blueprint("dashboard", __name__)
+# we name the blueprint dashboard
+# the template folder is called templates. File names passed to render_template are relative to this path
+dashboard = Blueprint("dashboard", __name__, template_folder="templates")
 
 @dashboard.route('/hello')
 def hello_dashboard():
     print("hello_dashboard")
-    return render_template("templates/hello_dashboard.html")
+    return render_template("hello_dashboard.html")
 
+@dashboard.route('/hello_vue')
+def hello_vue():
+    print("hello_vue")
+    return send_from_directory("views/templates", "hello_vue.html")
 # End of file dashboard.py
